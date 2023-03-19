33df0b75fda110eea0638c72d98950e667a04e84
Nick Chikovani <nickeof@gmail.com>
2023-03-19 04:52:03 +0400

    load user balance

--- VIRAL/js/custom.js
+++ VIRAL/js/custom.js
@@ -4,75 +4,93 @@ Author: Askbootstrap
 Author URI: https://themeforest.net/user/askbootstrap
 Version: 1.0
 */
-// const sdk = new ThirdwebSDK("mumbai");
-// const contract = await sdk.getContract("0x1833AC111fd43Ab9eD6917C521cd19E322899666");
-// const data = await contract.erc20.balance();
 
-// const data = await contract.call("balanceOf", account);
-// const balance = await contract.erc20.balance();
+async function main() {
+  const accounts = await window.ethereum.request({
+    method: "eth_requestAccounts",
+  });
+  const sdk = new ThirdwebSDK("mumbai");
+  const contract = await sdk.getContract(
+    "0x1833AC111fd43Ab9eD6917C521cd19E322899666"
+  );
+  const data = await contract.erc20.balanceOf(accounts[0]);
+  console.log("Your balance is: " + data.value.toString());
+}
 
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Toggle the side navigation
-   $(document).on('click', '#sidebarToggle', function(e) {  
+  $(document).on("click", "#sidebarToggle", function (e) {
     e.preventDefault();
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
   });
 
   $("#connect-button").click(() => {
-    connectWallet()
-  })
+    connectWallet();
+  });
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
-    if ($window.width() > 768) {
-      var e0 = e.originalEvent,
-        delta = e0.wheelDelta || -e0.detail;
-      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-      e.preventDefault();
+  $("body.fixed-nav .sidebar").on(
+    "mousewheel DOMMouseScroll wheel",
+    function (e) {
+      if ($window.width() > 768) {
+        var e0 = e.originalEvent,
+          delta = e0.wheelDelta || -e0.detail;
+        this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+        e.preventDefault();
+      }
     }
-  });
-  
+  );
+
   // Category Owl Carousel
   var objowlcarousel = $(".owl-carousel-category");
   if (objowlcarousel.length > 0) {
-	 objowlcarousel.owlCarousel({
-		items: 8,
-		lazyLoad: true,
-		pagination: false,
-		loop: true,
-		autoPlay: 2000,
-		navigation: true,
-		stopOnHover: true,
-		navigationText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"]
-	});
+    objowlcarousel.owlCarousel({
+      items: 8,
+      lazyLoad: true,
+      pagination: false,
+      loop: true,
+      autoPlay: 2000,
+      navigation: true,
+      stopOnHover: true,
+      navigationText: [
+        "<i class='fa fa-chevron-left'></i>",
+        "<i class='fa fa-chevron-right'></i>",
+      ],
+    });
   }
 
-	
   // Tooltip
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
 
   // Scroll to top button appear
-  $(document).on('scroll', function() {
+  $(document).on("scroll", function () {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
+      $(".scroll-to-top").fadeIn();
     } else {
-      $('.scroll-to-top').fadeOut();
+      $(".scroll-to-top").fadeOut();
     }
   });
 
   // Smooth scrolling using jQuery easing
-  $(document).on('click', 'a.scroll-to-top', function(event) {
+  $(document).on("click", "a.scroll-to-top", function (event) {
     var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top)
-    }, 1000, 'easeInOutExpo');
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top,
+        },
+        1000,
+        "easeInOutExpo"
+      );
     event.preventDefault();
   });
 
+  main();
 })(jQuery); // End of use strict
 
 // metamask wallet
@@ -81,40 +99,42 @@ async function connectWallet() {
   if (window.ethereum) {
     try {
       // Request account access
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      console.log('Connected to Metamask!');
-      
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected to Metamask!");
+
       // Hide connect button and show wallet address
       document.getElementById("connect-button").style.display = "none";
       document.getElementById("wallet-address").textContent = accounts[0];
       document.getElementById("wallet-address").style.display = "block";
-      
     } catch (error) {
       console.error(error);
     }
   } else {
-    console.log('Please install Metamask to connect to this website!');
+    console.log("Please install Metamask to connect to this website!");
   }
 }
 
- // get the button and span elements using their IDs
- var showTextBtn = document.getElementById("showTextBtn");
- var mySpan = document.getElementById("mySpann");
- 
- var num = 0;
- // add a click event listener to the button
- showTextBtn.addEventListener("click", function() {
-    if (num%2==0) { // check if it is currently hidden
-       mySpan.style.display = "inline";
-       mySpan.innerText = data;
-      
- // show the text
-    } else {
-       mySpan.style.display = "none"; // hide the text
-    }
- });
+// get the button and span elements using their IDs
+var showTextBtn = document.getElementById("showTextBtn");
+var mySpan = document.getElementById("mySpann");
 
- const waitButton = document.getElementById("showTextBtn");
+var num = 0;
+// add a click event listener to the button
+showTextBtn.addEventListener("click", function () {
+  if (num % 2 == 0) {
+    // check if it is currently hidden
+    mySpan.style.display = "inline";
+    mySpan.innerText = data;
+
+    // show the text
+  } else {
+    mySpan.style.display = "none"; // hide the text
+  }
+});
+
+const waitButton = document.getElementById("showTextBtn");
 const waitOverlay = document.getElementById("wait-overlay");
 
 waitButton.addEventListener("click", () => {
@@ -127,9 +147,8 @@ waitButton.addEventListener("click", () => {
   }, 5000);
 });
 
-const myInput = document.getElementById('showTextBtn');
+const myInput = document.getElementById("showTextBtn");
 
-myInput.addEventListener('click', () => {
-  myInput.value = '';
+myInput.addEventListener("click", () => {
+  myInput.value = "";
 });
-
